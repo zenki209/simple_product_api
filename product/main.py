ba28afc93908d28d8208bbbafbe50d971a001526
unknown <khoa.trinh@hotmail.com>
2024-02-18 15:04:36 +0700

    adding data into the databases

--- product/main.py
+++ product/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi.params import Depends
 from schemas import *
-from database import Base, engine
+from database import Base, engine, SessionLocal
+from sqlalchemy.orm import Session
+from sqlalchemy.sql.functions import mode
 import models
 
 app = FastAPI()
@@ -8,7 +11,21 @@ app = FastAPI()
 models.Base.metadata.create_all(engine)
 
 
-@app.post('/products')
-def add(request: Product):
-    return request
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
+
+@app.post('/products')
+def add(request: Product, db: Session = Depends(get_db)):
+    new_product = models.Product(
+        name=request.name, description=request.description, price=request.price)
+    print(db)
+    print(request.name)
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return request
