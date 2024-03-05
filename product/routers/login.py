e0c01cb8f69935c0c87945a6cc5e8686d403c8ea
unknown <khoa.trinh@hotmail.com>
2024-03-06 02:32:14 +0700

    adding seketon get current user

--- product/routers/login.py
+++ product/routers/login.py
@@ -5,13 +5,15 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import get_db
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
+
+from fastapi.security import OAuth2PasswordBearer
 
 # FOR JWT
 SECRET_KEY = "f9037d2829bb8ae55a34f42b5969e8a1d23fb732e86bcaf18be6a8543c941fe8e4847d199f8b6b035452d6c2f733407dbafbffab4884e27b09c579027a1f2b1699554ed512e87c80ef94a940fd455d19"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 20
-
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 router = APIRouter()
 
@@ -27,6 +29,21 @@ def generate_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credetinals_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid USer"
+        headers={'WWW-Authenticate':"Bearer"}
+    )
+    try:
+        payload = jwt.decode(token,SECRET_KEY= SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if username is None:
+            pass
+        token_data = schemas.TokenData(username=username)
+    except JWTError:
+        raise credetinals_exception
 
 @router.post('/login')
 def login(request: schemas.Login, db: Session = Depends(get_db)):
